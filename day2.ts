28479ba21aba048649a21c75e923ef7ffd597c0d
Fame <SlavaNek@yandex.ru>
2018-12-01 09:47:00 +0200

    day1

--- day2.ts
+++ day2.ts
@@ -1,6 +1,6 @@
 import {getInput, getTestFunction} from './helper';
 
-const DAY = 1;
+const DAY = 2;
 
 tests();
 run().then(([result1, result2]) => {
@@ -8,27 +8,19 @@ run().then(([result1, result2]) => {
   console.log('Part 2:', result2);
 });
 
-function calculatePart1(input: number[]) {
-  return input.reduce((buf, value) => buf+=value);
+function calculatePart1(input: any) {
+  
 }
 
-function calculatePart2(input: number[]) {
-  const arr = [0];
-  let value = 0;
-  while (true) {
-    for (const i of input) {
-      value += i;
-      if (arr.indexOf(value) >= 0) {
-        return value;
-      }
-      arr.push(value)
-    }
-  }
+function calculatePart2(input: any) {
+ 
 }
 
-function parse(input: string): number[] {
+function parse(input: string): number[][] {
+  const regexp = /\d+/g;
   return input.split('\n')
-    .map(row => +row)
+    .map(row => row.match(regexp)
+      .map(val => +val))
 }
 
 export async function run() {
@@ -42,14 +34,8 @@ function tests() {
   const part1Test = getTestFunction((input) => calculatePart1(input));
   const part2Test = getTestFunction((input) => calculatePart2(input));
   part1Test([1, 1, 1], 3);
-  part1Test([1, 1, -2], 0);
-  part1Test([1, 1, -2], 0);
-
   console.log('---------------------');
 
   part2Test([+1, -1], 0);
-  part2Test([+3, +3, +4, -2, -4], 10);
-  part2Test([-6, +3, +8, +5, -6], 5);
-  part2Test([+7, +7, -2, -7, -4], 14);
   console.log('---------------------');
 }
