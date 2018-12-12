abe72b62061a4c534b3f1a6e3e67e520e041b2ea
yaroslav <yaroslavnekryach@gmail.com>
2018-12-12 13:14:32 +0200

    day12 (fail)

--- day12.ts
+++ day12.ts
@@ -9,33 +9,107 @@ run().then(([result1, result2]) => {
 });
 
 function calculatePart1(input) {
-  
+  let zeroItem = 3;
+  let state = '...' + input[0] + '...';
+  const rules = input[1];
+  for (let i = 0; i < 20; i++) {
+    const newState = [];
+    for (let s = 0; s < state.length - 5; s++) {
+      const slice = state.slice(s, s + 5);
+      const val = rules[slice] || '.';
+      newState.push(val);
+    }
+    state = '....' + newState.join('') + '....';
+    zeroItem += 2;
+    console.log(state);
+  }
+  let result = 0;
+  for (let i = 0; i < state.length; i++) {
+    const char = state.charAt(i);
+    const index = i - zeroItem;
+    if (char === '#') {
+      result += index;
+    }
+  }
+  return getCount(state, zeroItem);
 }
 
 function calculatePart2(input) {
-
+  let zeroItem = 3;
+  let state = '...' + input[0] + '...';
+  let prevCount = 0;
+  let count = 0;
+  const rules = input[1];
+  for (let i = 0; i < 200; i++) {
+    count = 0;
+    const newState = [];
+    for (let s = 0; s < state.length - 5; s++) {
+      const slice = state.slice(s, s + 5);
+      const val = rules[slice] || '.';
+      if (val === '#') {
+        count++;
+      }
+      newState.push(val);
+    }
+    state = '....' + newState.join('') + '....';
+    zeroItem += 2;
+    const val = getCount(state, zeroItem);
+    console.log(i, val);
+  }
 }
 
-function parse(input: string): number[] {
-  const regexp = /\d+/;
-  return input.split('\n')
+function getCount(state: string, offset: number): number {
+  let result = 0;
+  for (let i = 0; i < state.length; i++) {
+    const char = state.charAt(i);
+    const index = i - offset;
+    if (char === '#') {
+      result += index;
+    }
+  }
+  return result;
+}
+
+function parse(input: string): [string, { [from: string]: string }] {
+  let [initial, data] = input.split('\n\n');
+  initial = initial.replace('initial state: ', '');
+  const regexp = /(.{5}) => (.)/;
+  const parsedData = {};
+  data.split('\n')
     .map(row => row.match(regexp))
-    .map(val => +val)
+    .map(val => parsedData[val[1]] = val[2]);
+  return [initial, parsedData];
 }
 
 export async function run() {
   const input: string = await getInput(DAY);
-  const result1 = calculatePart1(parse(input));
+  const result1 = '0';///calculatePart1(parse(input));
   const result2 = calculatePart2(parse(input));
-  return [result1, result2]
+  return [result1, result2];
 }
 
 function tests() {
   const part1Test = getTestFunction((input) => calculatePart1(parse(input)));
   const part2Test = getTestFunction((input) => calculatePart2(parse(input)));
-  part1Test([], 0);
+  part1Test(`initial state: #..#.#..##......###...###
+
+...## => #
+..#.. => #
+.#... => #
+.#.#. => #
+.#.## => #
+.##.. => #
+.#### => #
+#.#.# => #
+#.### => #
+##.#. => #
+##.## => #
+###.. => #
+###.# => #
+####. => #`, 325);
   console.log('---------------------');
 
-  part2Test([], 0);
+  // part2Test([], 0);
   console.log('---------------------');
 }
+
