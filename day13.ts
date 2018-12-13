d4a0331d0161441cd56eb3841f55282879116f36
Fame <SlavaNek@yandex.ru>
2018-12-13 23:05:04 +0200

    day13 upgrade

--- day13.ts
+++ day13.ts
@@ -3,17 +3,42 @@ import {getInput, getTestFunction} from './helper';
 const DAY = 13;
 
 enum Direction {LEFT, UP, RIGHT, DOWN}
-enum Turn {
-  LEFT = -1,
-  STRAIGHT = 0,
-  RIGHT = 1
-}
+const TURN  = [-1, 0, 1];
+
+const INIT_CATS_DIRECTIONS = {
+  '<': Direction.LEFT,
+  '>': Direction.RIGHT,
+  'v': Direction.DOWN,
+  '^': Direction.UP
+};
+
+const DIRECTION_OFFSET = {
+  [Direction.LEFT]: {x: -1, y: 0},
+  [Direction.RIGHT]: {x: 1, y: 0},
+  [Direction.DOWN]: {x: 0, y: 1},
+  [Direction.UP]: {x: 0, y: -1},
+};
+
+const TURN_MAP = {
+  '/': {
+    [Direction.LEFT]: Direction.DOWN,
+    [Direction.DOWN]: Direction.LEFT,
+    [Direction.RIGHT]: Direction.UP,
+    [Direction.UP]: Direction.RIGHT
+  },
+  '\\': {
+    [Direction.LEFT]: Direction.UP,
+    [Direction.UP]: Direction.LEFT,
+    [Direction.RIGHT]: Direction.DOWN,
+    [Direction.DOWN]: Direction.RIGHT
+  }
+};
 
 class Car {
   x: number;
   y: number;
   direction: Direction;
-  nextTurn: Turn = Turn.LEFT;
+  turnCount: number = 0;
   
   constructor(x: number, y: number, direction: Direction) {
     this.x = x;
@@ -25,44 +50,18 @@ class Car {
     return `${this.x},${this.y}`;
   }
   
-  move(next: string): string {
-    if (this.direction === Direction.LEFT) {
-      this.x--;
-    } else if(this.direction === Direction.RIGHT) {
-      this.x++;
-    } else if(this.direction === Direction.UP) {
-      this.y--;
-    } else if(this.direction === Direction.DOWN) {
-      this.y++;
-    }
-    
-    if (next === '+') {
-      this.direction += this.nextTurn + 4;
-      this.direction %= 4;
-      this.nextTurn++;
-      if (this.nextTurn > 1) {
-        this.nextTurn = -1;
-      }
-    } else if (next === '/') {
-      if (this.direction === Direction.LEFT) {
-        this.direction = Direction.DOWN;
-      } else if (this.direction === Direction.RIGHT) {
-        this.direction = Direction.UP;
-      } else if (this.direction === Direction.UP) {
-        this.direction = Direction.RIGHT;
-      } else if (this.direction === Direction.DOWN) {
-        this.direction = Direction.LEFT;
-      }
-    } else if (next === '\\') {
-      if (this.direction === Direction.LEFT) {
-        this.direction = Direction.UP;
-      } else if (this.direction === Direction.RIGHT) {
-        this.direction = Direction.DOWN;
-      } else if (this.direction === Direction.UP) {
-        this.direction = Direction.LEFT;
-      } else if (this.direction === Direction.DOWN) {
-        this.direction = Direction.RIGHT;
-      }
+  move(input: string[][]): string {
+    const offset = DIRECTION_OFFSET[this.direction];
+    this.x += offset.x;
+    this.y += offset.y;
+    const tile = input[this.y][this.x];
+
+    if (tile === '+') {
+      this.direction = this.direction + TURN[this.turnCount % 3];
+      this.direction = (this.direction  + 4) % 4;
+      this.turnCount++;
+    } else if (tile === '/' || tile === '\\') {
+      this.direction = TURN_MAP[tile][this.direction];
     }
     
     return this.getPos();
@@ -71,48 +70,30 @@ class Car {
   
 
 
-// tests();
+tests();
 run().then(([result1, result2]) => {
   console.log('Part 1:', result1);
   console.log('Part 2:', result2);
 });
 
-function calculatePart1(input: string[][]) {
+function getCars(input: string[][]): Car[] {
   const cars: Car[] = [];
   for (let y = 0; y < input.length; y++) {
     for (let x = 0; x < input[y].length; x++) {
-      if (input[y][x] === '<') {
-        const newCar = new Car(x, y, Direction.LEFT);
-        cars.push(newCar);
-        input[y][x] = '-'
-      } else if (input[y][x] === '>') {
-        const newCar = new Car(x, y, Direction.RIGHT);
-        cars.push(newCar);
-        input[y][x] = '-'
-      } else if (input[y][x] === 'v') {
-        const newCar = new Car(x, y, Direction.DOWN);
-        cars.push(newCar);
-        input[y][x] = '|';
-      } else if (input[y][x] === '^') {
-        const newCar = new Car(x, y, Direction.UP);
-        cars.push(newCar);
-        input[y][x] = '|';
+      if (INIT_CATS_DIRECTIONS[input[y][x]] != undefined) {
+        cars.push( new Car(x, y, INIT_CATS_DIRECTIONS[input[y][x]]));
       }
     }
   }
-  
+  return cars;
+}
+
+function calculatePart1(input: string[][]) {
+  const cars: Car[] = getCars(input);
   while (true) {
     cars.sort((a, b) => a.y - b.y === 0 ? a.x - b.x : a.y - b.y);
     for (const car of cars) {
-      if (car.direction === Direction.UP) {
-        car.move(input[car.y - 1][car.x]);
-      } else if (car.direction === Direction.DOWN) {
-        car.move(input[car.y + 1][car.x]);
-      } else if (car.direction === Direction.LEFT) {
-        car.move(input[car.y][car.x - 1]);
-      } else if (car.direction === Direction.RIGHT) {
-        car.move(input[car.y][car.x + 1]);
-      }
+      car.move(input);
       
       const newPos = car.getPos();
       const carsAtPos = cars.filter(car => car.getPos() === newPos);
@@ -124,53 +105,24 @@ function calculatePart1(input: string[][]) {
 }
 
 function calculatePart2(input) {
-  const cars: Car[] = [];
-  for (let y = 0; y < input.length; y++) {
-    for (let x = 0; x < input[y].length; x++) {
-      if (input[y][x] === '<') {
-        const newCar = new Car(x, y, Direction.LEFT);
-        cars.push(newCar);
-        input[y][x] = '-'
-      } else if (input[y][x] === '>') {
-        const newCar = new Car(x, y, Direction.RIGHT);
-        cars.push(newCar);
-        input[y][x] = '-'
-      } else if (input[y][x] === 'v') {
-        const newCar = new Car(x, y, Direction.DOWN);
-        cars.push(newCar);
-        input[y][x] = '|';
-      } else if (input[y][x] === '^') {
-        const newCar = new Car(x, y, Direction.UP);
-        cars.push(newCar);
-        input[y][x] = '|';
-      }
-    }
-  }
-  const crashedCars = [];
+  let cars: Car[] = getCars(input);
+  
   while (true) {
+    const crashedCars = [];
     cars.sort((a, b) => a.y - b.y === 0 ? a.x - b.x : a.y - b.y);
     for (const car of cars) {
-      if (crashedCars.indexOf(car) >= 0) {
-        continue;
-      }
-      if (car.direction === Direction.UP) {
-        car.move(input[car.y - 1][car.x]);
-      } else if (car.direction === Direction.DOWN) {
-        car.move(input[car.y + 1][car.x]);
-      } else if (car.direction === Direction.LEFT) {
-        car.move(input[car.y][car.x - 1]);
-      } else if (car.direction === Direction.RIGHT) {
-        car.move(input[car.y][car.x + 1]);
-      }
-
-      const newPos = car.getPos();
-      const carsAtPos = cars.filter(car => car.getPos() === newPos && crashedCars.indexOf(car) < 0);
-      if (carsAtPos.length > 1) {
-        crashedCars.push(...carsAtPos);
+      if (crashedCars.indexOf(car) < 0) {
+        car.move(input);
+        const newPos = car.getPos();
+        const carsAtPos = cars.filter(car => car.getPos() === newPos);
+        if (carsAtPos.length > 1) {
+          crashedCars.push(...carsAtPos);
+        }
       }
     }
-    if (cars.length - crashedCars.length === 1) {
-      return cars.find(car => crashedCars.indexOf(car) < 0).getPos();
+    cars = cars.filter(car => crashedCars.indexOf(car) < 0);
+    if (cars.length === 1) {
+      return cars[0].getPos();
     }
   }
 }
