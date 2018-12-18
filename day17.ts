21973605fcbd8a3d1acbf68c1a4b3733e2f4fea5
Fame <SlavaNek@yandex.ru>
2018-12-18 07:43:04 +0200

    day18 need to upgrade

--- day17.ts
+++ day17.ts
@@ -3,39 +3,125 @@ import {getInput, getTestFunction} from './helper';
 const DAY = 17;
 
 tests();
-run().then(([result1, result2]) => {
-  console.log('Part 1:', result1);
-  console.log('Part 2:', result2);
-});
+// run().then(([result1, result2]) => {
+//   console.log('Part 1:', result1);
+//   console.log('Part 2:', result2);
+// });
 
-function calculatePart1(input) {
+function calculatePart1(input: {[cord: string]: string}, min: number, max: number) {
+  console.log(input);
+  console.log(min);
+  console.log(max);
+  fillWithSpring(input, 500, 0, min, max);
+  let result = 0;
+  for (const coord in input) {
+    if (input[coord] === '~') {
+      result++
+    }
+  }
+  return result;
+}
 
+function fillWithSpring(map: { [cord: string]: string }, xs: number, ys: number, min: number, max: number) {
+  let bottom;
+  for (let y = ys; y <= max; y++) {
+    if (map[coordToStr(xs, y)] === '#') {
+      bottom = y - 1;
+      break;
+    } else {
+      map[coordToStr(xs, y)] === '~';
+    }
+  }
+  if (bottom) {
+    let go = true;
+    while (bottom >= min && bottom >= ys && go) {
+      for (let x = xs; ; x--) {
+        if (map[coordToStr(x, bottom)] === '#') {
+          break;
+        }
+        if (map[coordToStr(x, bottom + 1)] !== '#') {
+          fillWithSpring(map, x, bottom, min, max);
+          map[coordToStr(x, bottom)] = '~';
+          go = false;
+          break;
+        }
+        map[coordToStr(x, bottom)] = '~';
+      }
+      for (let x = xs; ; x++) {
+        if (map[coordToStr(x, bottom)] === '#') {
+          break;
+        }
+        if (map[coordToStr(x, bottom + 1)] !== '#') {
+          fillWithSpring(map, x, bottom, min, max);
+          map[coordToStr(x, bottom)] = '~';
+          go = false;
+          break;
+        }
+        map[coordToStr(x, bottom)] = '~';
+      }
+      bottom--;
+    }
+  }
 }
 
 function calculatePart2(input) {
 
 }
 
-function parse(input: string): number[] {
-  const regexp = /\d+/;
-  return input.split('\n')
+function coordToStr(x: number, y: number): string {
+  return `${x},${y}`;
+}
+
+function parse(input: string): [{[cord: string]: string}, number, number] {
+  let result = {};
+  const regexp = /([xy])=(\d+), ([xy])=(\d+)\.\.(\d+)/;
+  let max = -Infinity;
+  let min = Infinity;
+  input.split('\n')
     .map(row => row.match(regexp))
-    .map(val => +val)
+    .forEach(val => {
+      if (val[1] === 'y') {
+        if (+val[2] > max) {
+          max = +val[2]
+        }
+        if (+val[2] < min) {
+          min = +val[2]
+        }
+      } else {
+        if (+val[5] > max) {
+          max = +val[5]
+        }
+        if (+val[4] < min) {
+          min = +val[4]
+        }
+      }
+      for (let i = +val[4]; i <= +val[5]; i++) {
+        result[val[1] === 'x' ? coordToStr(+val[2], i) : coordToStr(i, +val[2])] = '#'
+      }
+    });
+  return [result, min, max];
 }
 
 export async function run() {
   const input: string = await getInput(DAY);
-  const result1 = calculatePart1(parse(input));
+  const result1 = calculatePart1(...parse(input));
   const result2 = calculatePart2(parse(input));
   return [result1, result2]
 }
 
 function tests() {
-  const part1Test = getTestFunction((input) => calculatePart1(parse(input)));
+  const part1Test = getTestFunction((input) => calculatePart1(...parse(input)));
   const part2Test = getTestFunction((input) => calculatePart2(parse(input)));
-  part1Test([], 0);
+  part1Test(`x=495, y=2..7
+y=7, x=495..501
+x=501, y=3..7
+x=498, y=2..4
+x=506, y=1..2
+x=498, y=10..13
+x=504, y=10..13
+y=13, x=498..504`, 57);
   console.log('---------------------');
 
-  part2Test([], 0);
+  // part2Test([], 0);
   console.log('---------------------');
 }
--- day18.ts
+++ day18.ts
@@ -2,25 +2,102 @@ import {getInput, getTestFunction} from './helper';
 
 const DAY = 18;
 
-tests();
+// tests();
 run().then(([result1, result2]) => {
   console.log('Part 1:', result1);
   console.log('Part 2:', result2);
 });
 
-function calculatePart1(input) {
+function calculatePart1(input: string[][]) {
+  for (let i = 0; i < 10; i++) {
+    const newInput = [];
+    for (let y = 0; y < input.length; y++) {
+      const newRow = [];
+      for (let x = 0; x < input.length; x++) {
+        const newValue = calculate(input, x, y);
+        newRow.push(newValue);
+      }
+      newInput.push(newRow)
+    }
+    console.log(i + 1);
+    console.log(newInput.map(row => row.join('')).join('\n'));
+    input = newInput;
+  }
 
+  let trees = 0;
+  let lumberyard = 0;
+  
+  for (let y = 0; y < input.length; y++) {
+    for (let x = 0; x < input.length; x++) {
+      if (input[y][x] === '|') {
+        trees++
+      }
+      if (input[y][x] === '#') {
+        lumberyard++
+      } 
+    }
+  }
+  return trees * lumberyard;
+}
+
+function calculate(input: string[][], x: number, y: number) {
+  const currVal = input[y][x];
+  const count = {'.': 0, '|': 0, '#': 0};
+  for (let i = y - 1; i <= y + 1; i++) {
+    for (let j = x - 1; j <= x + 1; j++) {
+      if (!input[i] || !input[i][j] || (i === y && j === x)) {
+        continue;
+      }
+      count[input[i][j]]++;
+    }
+  }
+  
+  if (currVal === '.' && count['|'] >= 3) {
+    return '|'
+  }
+  if (currVal === '|' && count['#'] >= 3) {
+    return '#'
+  }
+  if (currVal === '#' && (count['#'] < 1 || count['|'] < 1)) {
+    return '.'
+  }
+  return currVal;
 }
 
 function calculatePart2(input) {
-
+  let trees = 0;
+  let lumberyard = 0;
+  for (let i = 0; i < 1000; i++) {
+    trees = 0;
+    lumberyard = 0;
+    const newInput = [];
+    for (let y = 0; y < input.length; y++) {
+      const newRow = [];
+      for (let x = 0; x < input.length; x++) {
+        const newValue = calculate(input, x, y);
+        if (newValue === '|') {
+          trees++
+        }
+        if (newValue === '#') {
+          lumberyard++
+        }
+        newRow.push(newValue);
+      }
+      newInput.push(newRow)
+    }
+    console.log(i + 1, trees * lumberyard);
+    // console.log(trees * lumberyard);
+    // console.log(newInput.map(row => row.join('')).join('\n'));
+    input = newInput;
+  }
+  
+  return trees * lumberyard;
 }
 
-function parse(input: string): number[] {
+function parse(input: string): string[][] {
   const regexp = /\d+/;
   return input.split('\n')
-    .map(row => row.match(regexp))
-    .map(val => +val)
+    .map(row => row.split(''))
 }
 
 export async function run() {
@@ -33,9 +110,18 @@ export async function run() {
 function tests() {
   const part1Test = getTestFunction((input) => calculatePart1(parse(input)));
   const part2Test = getTestFunction((input) => calculatePart2(parse(input)));
-  part1Test([], 0);
+  part1Test(`.#.#...|#.
+.....#|##|
+.|..|...#.
+..|#.....#
+#.#|||#|#|
+...#.||...
+.|....|...
+||...#|.#|
+|.||||..|.
+...#.|..|.`, 1147);
   console.log('---------------------');
 
-  part2Test([], 0);
+  // part2Test([], 0);
   console.log('---------------------');
 }
